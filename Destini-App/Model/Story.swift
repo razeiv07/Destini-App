971fef9b7bbf60ba008cb3a2013527ad707e0849
razeiv07 <rajeevxrma@gmail.com>
2024-07-31 21:57:02 -0400

    UI created with properly maintained constraints

---
+++ Destini-App/Model/Story.swift
@@ -0,0 +1,8 @@
+//
+//  Story.swift
+//  Destini-App
+//
+//  Created by Rajeev Sharma on 2024-07-30.
+//
+
+import Foundation
---
+++ Destini-App/Model/StoryBrain.swift
@@ -0,0 +1,8 @@
+//
+//  StoryBrain.swift
+//  Destini-App
+//
+//  Created by Rajeev Sharma on 2024-07-30.
+//
+
+import Foundation
