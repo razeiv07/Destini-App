c65ab1cf3895d5e11fa6155b130be82b8017049b
razeiv07 <rajeevxrma@gmail.com>
2024-09-02 16:35:38 -0400

    Implement StoryBrain model
    - Add getStoryText, getChoice1, and getChoice2 functions
    - Include function to check user's answer with the story's answer
    - Update the UI for improved user interaction

--- Destini-App/Controller/ViewController.swift
+++ Destini-App/Controller/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        storyLabel.text = storyBrain.getStoryText()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+        
     }
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle!
+        storyBrain.chcekAnswer(userAnswer)
 
-
+        storyLabel.text = storyBrain.getStoryText()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
 }
 
